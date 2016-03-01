a25d58ebcac22e6dc4e35a13ec1b3c989a51cf72
Michael Campos <camposmj89@gmail.com>
2016-03-01 13:37:33 -0800

    merging

---
+++ db/migrate/20160301212847_add_columns_to_students.rb
@@ -0,0 +1,9 @@
+class AddColumnsToStudents < ActiveRecord::Migration[5.0]
+	def change
+		add_column :students, :first_name, :string
+		add_column :students, :last_name, :string
+		add_column :students, :email, :string
+		add_column :students, :password_digest, :string
+		add_column :students, :phone_number, :string
+	end
+end
